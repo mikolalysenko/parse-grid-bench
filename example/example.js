43cd3747143623585bc0d6943ad7f863c5207ecd
mikolalysenko <mikolalysenko@gmail.com>
2015-04-25 09:37:45 -0500

    update example

--- example/example.js
+++ example/example.js
@@ -6,8 +6,16 @@ var imshow = require('ndarray-imshow')
 var parseMap = require('../map')
 var parseScen = require('../scenario')
 
-var mapStr = fs.readFileSync(path.join(__dirname, '../data/maps/bg512/AR0011SR.map'), 'utf-8')
-var scenStr = fs.readFileSync(path.join(__dirname, '../data/scenarios/bg512/AR0011SR.map.scen'), 'utf-8')
+var mapStr = fs.readFileSync(path.join(__dirname, '../data/maps/arena2.map'), 'utf-8')
+var scenStr = fs.readFileSync(path.join(__dirname, '../data/scenarios/arena2.map.scen'), 'utf-8')
 
-console.log(parseScen(scenStr))
-imshow(parseMap(mapStr))
+
+var map       = parseMap(mapStr)
+var scenarios = parseScen(scenStr)
+
+//Plot points and show image
+for(var i=0; i<scenarios.length; ++i) {
+  map.set(scenarios[i].srcY, scenarios[i].srcX, 2)
+  map.set(scenarios[i].dstY, scenarios[i].dstX, 3)
+}
+imshow(map)
